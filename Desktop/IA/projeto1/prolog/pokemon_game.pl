7b3162870a98ec7fe7c80d31eb9fe1fac53ba685
João Valério <joaosavalerio@gmail.com>
2026-03-31 23:22:39 +0100

    pokemon gamepl

--- Desktop/IA/projeto1/prolog/pokemon_game.pl
+++ Desktop/IA/projeto1/prolog/pokemon_game.pl
@@ -3,14 +3,33 @@
 :-ensure_loaded("pokemon_route.pl").
 
 player_starts(0,0).
-% Caso Base: Se o índice for 0, o elemento é a Cabeça (H).
-obter_at_index(0, [H|_], H).
 
-% Caso Recursivo: Se o índice for N (MAIOR que 0), continua a procurar.
-obter_at_index(N, [_|T], E) :-
-    N > 0,              % Aqui garantimos que se N for 0, esta regra nem tenta correr
+% predicado auxiliar para ir buscar a informação de um pokemon num indice 
+
+% caso base (indice 0)
+get_at_index(0, [H|_], H).
+
+% caso recursivo ( usamos tail e retiramos a head em cada iteracão)
+get_at_index(N, [_|T], E) :-
+    N > 0,        
     N1 is N - 1,
-    obter_at_index(N1, T, E).
+    get_at_index(N1, T, E).
+
+
+
+% posicões adjacentes com verificação outOfBounds
+adjacent(X, Y, NX, Y) :- NX is X + 1, NX =< 4. % Right
+adjacent(X, Y, NX, Y) :- NX is X - 1, NX >= 0. % Left
+adjacent(X, Y, X, NY) :- NY is Y + 1, NY =< 4. % Down
+adjacent(X, Y, X, NY) :- NY is Y - 1, NY >= 0. % Up
+
+
+% ir buscar a informacão a 
+get_cell(X, Y, Matrix, CellData) :-
+    get_at_index(Y, Matrix, Row),
+    get_at_index(X, Row, CellData).
+
+
 % TO DO
 next_rooms(X,Y,Rooms) :-
     route(M).
